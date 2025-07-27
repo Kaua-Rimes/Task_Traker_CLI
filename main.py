3e7bb05da42cadd892d2560e083905f7d5df0bb1
Kauã <kauarimes15@gmail.com>
2025-07-27 13:28:12 -0300

    Create main.py
    Create main.py file with the initial prints for the main menu.

---
+++ main.py
@@ -0,0 +1,13 @@
+from colorama import init, Fore, Style
+
+init(autoreset=True)
+
+print(Fore.CYAN + Style.BRIGHT + "WELCOME TO TASK TRACKER CLI! WHAT WOULD YOU LIKE TO DO?")
+print(Fore.YELLOW + "1. Add a new task")
+print(Fore.YELLOW + "2. View all tasks")
+print(Fore.YELLOW + "3. Mark a task as completed")
+print(Fore.YELLOW + "4. Delete a task")
+print(Fore.RED + "5. Exit")
+
+choice = input(Fore.GREEN + "Please enter your choice [1-5]: ")
+
